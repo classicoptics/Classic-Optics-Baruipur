12ef95b58f0a678a64fe958f0981ebdb36261567
Classic Optics <classicoptics11@gmail.com>
2025-02-02 14:00:37 +0530

    Classic Optics V2.0 Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bar = document.getElementById('bar');
+/*const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
@@ -12,4 +12,132 @@ if (close){
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}*/
+
+
+
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+        // Add show-icon to show and hide menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+ 
+ /*=============== SHOW DROPDOWN MENU ===============*/
+ const dropdownItems = document.querySelectorAll('.dropdown__item')
+ 
+ // 1. Select each dropdown item
+ dropdownItems.forEach((item) =>{
+     const dropdownButton = item.querySelector('.dropdown__button') 
+ 
+     // 2. Select each button click
+     dropdownButton.addEventListener('click', () =>{
+         // 7. Select the current show-dropdown class
+         const showDropdown = document.querySelector('.show-dropdown')
+         
+         // 5. Call the toggleItem function
+         toggleItem(item)
+ 
+         // 8. Remove the show-dropdown class from other items
+         if(showDropdown && showDropdown!== item){
+             toggleItem(showDropdown)
+         }
+     })
+ })
+ 
+ // 3. Create a function to display the dropdown
+ const toggleItem = (item) =>{
+     // 3.1. Select each dropdown content
+     const dropdownContainer = item.querySelector('.dropdown__container')
+ 
+     // 6. If the same item contains the show-dropdown class, remove
+     if(item.classList.contains('show-dropdown')){
+         dropdownContainer.removeAttribute('style')
+         item.classList.remove('show-dropdown')
+     } else{
+         // 4. Add the maximum height to the dropdown content and add the show-dropdown class
+         dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+         item.classList.add('show-dropdown')
+     }
+ }
+ 
+ /*=============== DELETE DROPDOWN STYLES ===============*/
+ const mediaQuery = matchMedia('(min-width: 1118px)'),
+       dropdownContainer = document.querySelectorAll('.dropdown__container')
+ 
+ // Function to remove dropdown styles in mobile mode when browser resizes
+ const removeStyle = () =>{
+     // Validate if the media query reaches 1118px
+     if(mediaQuery.matches){
+         // Remove the dropdown container height style
+         dropdownContainer.forEach((e) =>{
+             e.removeAttribute('style')
+         })
+ 
+         // Remove the show-dropdown class from dropdown item
+         dropdownItems.forEach((e) =>{
+             e.classList.remove('show-dropdown')
+         })
+     }
+ }
+ 
+ addEventListener('resize', removeStyle)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+    }
+      slideIndex++;
+         if (slideIndex > slides.length) { slideIndex = 1 }
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
